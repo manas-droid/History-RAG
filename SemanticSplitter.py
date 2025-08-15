f21a91ca8150178e7b2b9e5028e34443a8a9cb2c
manas-droid <manas.kalangan@gmail.com>
2025-08-15 17:41:55 -0400

    Added semantic chunker

---
+++ SemanticSplitter.py
@@ -0,0 +1,16 @@
+
+import BaseSplitter as bs
+from typing import List
+from langchain_core.documents import Document
+from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
+
+
+
+class SemanticSplitter(bs.BaseSplitter):
+    def get_chunks(self, texts:List[str])->List[Document]:
+        embeddings = HuggingFaceEndpointEmbeddings(model="BAAI/bge-small-en-v1.5")
+        text_splitter = SemanticChunker(embeddings)
+        docs = text_splitter.create_documents(texts)
+        return docs
+    
