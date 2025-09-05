b07662ebfd12909d3a1b678e4d30166e64f98c04
manas-droid <manas.kalangan@gmail.com>
2025-09-05 17:26:07 -0400

    removed unnecessary code

--- data-scrapper-pdf.py
+++ data-scrapper-pdf.py
@@ -20,26 +20,9 @@ with open(file_name, 'r') as f:
             if pdf_obj:
                 pdf_url = pdf_obj['data']
                 pdf_title = pdf_url.split('/')[-1]
-                print(pdf_title)
                 result = requests.get(f"{base_url}{pdf_url}")
                 with open(f'./pdfs/{pdf_title}', 'wb') as f:
                     f.write(result.content)
-
-
-# with open(file_name, 'r') as f:
-#     data = json.load(f)
-
-#     for doc in data:
-#         document = doc['document'].replace('\n', '')
-#         link = doc['link']
-#         if not document:
-
-
-
-# result = requests.get("https://digitalarchive.wilsoncenter.org/sites/default/files/shared-media/imported_document_original_file/1944_11_25__Wilgress_to_Secretary_of_State.pdf")
-
-# with open('./pdfs/downloaded_document.pdf', 'wb') as f:
-#     f.write(result.content)
 
 
 
